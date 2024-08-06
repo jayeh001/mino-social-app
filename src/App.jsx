48a9a2035b17e21517f73d7d28bac57bb91a41bb
jayeh001 <68215119+jayeh001@users.noreply.github.com>
2024-08-06 16:57:58 -0700

    clean code

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import UserPage from "./pages/UserPage.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
 import RootLayout from "./RootLayout.jsx";
 import SignInPage from "./pages/SignInPage.jsx";
-import MainLayout from "./MainLayout.jsx";
+import MainLayout from "./pages/MainLayout.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 
 const theme = createTheme({
